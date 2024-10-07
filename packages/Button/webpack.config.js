9035c149c413320f0274e70e6e9fafc309c1f43d
shivam.trivedi.hs <shivam.trivedi.hs@gmail.com>
2024-10-07 11:08:07 +0000

    Monorepo init

---
+++ packages/Button/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    libraryTarget: 'umd',
+    globalObject: 'this',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  externals: {
+    react: 'react',
+    'react-dom': 'react-dom',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  mode: 'production',
+};
